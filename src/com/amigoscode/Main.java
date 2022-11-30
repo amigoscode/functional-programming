38745a91dcd0af695fdb9158d4ce697debbdbbe8
amigoscode <amigoscode@gmail.com>
2022-11-30 16:32:57 +0000

    Predicates

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,21 +1,28 @@
 package com.amigoscode;
 
-import java.util.function.BiConsumer;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
+        var email = "hello@amigoscode.com";
+        // System.out.println(isValidEmail(email));
+        System.out.println(isValidEmailPredicate.test(email));
 
-        sendEmailBiConsumer.accept(
-                "hello@amigoscode.com",
-                "alexu@gmail.com");
+        boolean test = isValidEmailPredicate
+                .and(containsDotPredicate).test(email);
+
+        System.out.println(test);
+
     }
 
-    static BiConsumer<String, String> sendEmailBiConsumer =
-            (from, to) ->
-                    System.out.printf("sending email from %s to %s%n", from, to);
+    static Predicate<String> isValidEmailPredicate =
+            email -> email.contains("@");
 
-    static void sendEmail(String from, String to) {
-        System.out.printf("sending email from %s to %s%n", from, to);
+    static Predicate<String> containsDotPredicate =
+            email -> email.contains(".");
+
+    static boolean isValidEmail(String email) {
+        return email.contains("@");
     }
 
 }
