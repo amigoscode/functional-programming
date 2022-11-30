dafe02e366d972548f8e3cf70a93c7d6b979093a
amigoscode <amigoscode@gmail.com>
2022-11-30 15:33:55 +0000

    BiFunction

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,23 +1,25 @@
 package com.amigoscode;
 
-import java.util.function.Function;
+import java.util.function.BiFunction;
 
 public class Main {
     public static void main(String[] args) {
-        Function<Integer, Integer> combinedFunction =
-                incrementByOneFunc.andThen(doubleFunc);
-
-        System.out.println(combinedFunction.apply(1));
-        System.out.println(combinedFunction.apply(3));
+        Person jamila = personMapperBiFunc.apply("Jamila", 18);
+        System.out.println(jamila);
     }
 
-    static Function<Integer, Integer> incrementByOneFunc =
-            n -> n + 1;
+    record Person(String name, int age){}
 
-    static Function<Integer, Integer> doubleFunc =
-            n -> n * 2;
+    static BiFunction<String, Integer, Person> personMapperBiFunc =
+            Person::new;
+    /*
+    static BiFunction<String, Integer, Person> personMapper =
+            (name, age) -> new Person(name, age);
 
-    static int incrementByOne (int n) {
-        return n + 1;
+    */
+
+    static Person personMapper(String name, int age) {
+        return new Person(name, age);
     }
+
 }
