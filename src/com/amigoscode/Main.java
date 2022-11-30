4b31db4f3ff87c9035134c21c3d147013cf9952f
amigoscode <amigoscode@gmail.com>
2022-11-30 16:02:49 +0000

    Consumer

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,25 +1,28 @@
 package com.amigoscode;
 
-import java.util.function.BiFunction;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
-        Person jamila = personMapperBiFunc.apply("Jamila", 18);
-        System.out.println(jamila);
+        // sendEmail("hello@amigoscode.com");
+        sendEmailConsumer.accept("jamila@amigoscode.com");
+
+        List<String> emails = List.of(
+                "hello@amigoscode.com",
+                "foo@amigoscode.com",
+                "bar@amigoscode.com"
+        );
+        emails.forEach(Main::sendEmail);
+        // emails.forEach(email -> sendEmail(email));
+
     }
 
-    record Person(String name, int age){}
+    static Consumer<String> sendEmailConsumer =
+            email -> System.out.println("sending email to " + email);
 
-    static BiFunction<String, Integer, Person> personMapperBiFunc =
-            Person::new;
-    /*
-    static BiFunction<String, Integer, Person> personMapper =
-            (name, age) -> new Person(name, age);
-
-    */
-
-    static Person personMapper(String name, int age) {
-        return new Person(name, age);
+    static void sendEmail(String email) {
+        System.out.println("sending email to " + email);
     }
 
 }
